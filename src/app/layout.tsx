50bad3b62c4c93bc534db91cc5486afa4b94a748
Dimsetrama <dimassanjaya2003@gmail.com>
2025-09-28 21:43:07 +0700

    oneday

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,8 @@
-import Footer from "@/app/_components/footer";
-import { ThemeSwitcher } from "./_components/theme-switcher";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import cn from "classnames";
-
+import { Footer } from "@/app/_components/footer";
+import { ThemeSwitcher } from "@/app/_components/theme-switcher";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -11,26 +10,17 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Etrama | Thrives on Neglect",
   description: "In order to shit fire, one must first chew glass.",
-  openGraph: {
-    images: ["/assets/og-image.png"],
-  },
-  icons: {
-    icon: "/favicon/favicon-32x32.png",
-    shortcut: "/favicon/favicon.ico",
-    apple: "/favicon/apple-touch-icon.png",
-  },
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={cn(inter.className, "dark:bg-slate-900 dark:text-slate-400")}
-      >
+      <body className={cn(inter.className, "dark:bg-slate-900 dark:text-slate-400")}>
+        <ThemeSwitcher />
         <div className="min-h-screen">{children}</div>
         <Footer />
       </body>
